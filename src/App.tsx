cbaed8ef7fd069b5b539270a48e7cba8809f732f
Krunal <krunal.purohit01@infosys.com>
2022-03-10 02:45:36 +0530

    connect backend env

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { API, graphqlOperation } from "aws-amplify";
+import { listTodos } from "./graphql/queries";
 
 function App() {
+  const [todos, setTodos] = useState<any>([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql<any>(graphqlOperation(listTodos));
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (err) {
+      console.log("error fetching todos");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Todo client app</p>
       </header>
+      <div style={styles.container as any}>
+        {todos.map((todo: any, index: number) => (
+          <div key={todo.id ? todo.id : index} style={styles.todo}>
+            <p style={styles.todoName}>Name: {todo.name}</p>
+            <p style={styles.todoDescription}>
+              Description: {todo.description}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
+const styles = {
+  container: {
+    width: 400,
+    margin: "5px auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: '8px 16px',
+    textAlign: "left",
+    border: "1px solid",
+  },
+  todo: { margin: 0 },
+  todoName: { fontSize: 20, fontWeight: "bold", margin: 0, marginBottom: 4 },
+  todoDescription: { margin: 0 },
+};
 
 export default App;
